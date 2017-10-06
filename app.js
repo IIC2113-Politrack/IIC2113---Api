61fac0d179fd79784a63cabe25341a9a45f3f745
Vicente Fuenzalida <vjfuenzalida@uc.cl>
2017-10-06 16:28:40 -0300

    Enables cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -39,6 +40,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable All CORS Requests
+app.use(cors())
 
 // use routes as middleware
 app.use('/', index);
